80a409177122ec1dd31ce74f7ec85013d639181c
Lucas Calheiros <lucascalheirosjllc@gmail.com>
2022-06-19 12:00:03 -0300

    Implement get file and save file from drive api

--- app/src/main/java/com/github/lucascalheiros/booklibrarymanager/useCase/GoogleDriveUseCase.kt
+++ app/src/main/java/com/github/lucascalheiros/booklibrarymanager/useCase/GoogleDriveUseCase.kt
@@ -6,7 +6,7 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.api.client.extensions.android.http.AndroidHttp
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential
-import com.google.api.client.http.ByteArrayContent
+import com.google.api.client.http.FileContent
 import com.google.api.client.json.gson.GsonFactory
 import com.google.api.services.drive.Drive
 import com.google.api.services.drive.DriveScopes
@@ -15,6 +15,8 @@ import com.google.api.services.drive.model.FileList
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import org.koin.core.annotation.Single
+import java.io.FileOutputStream
+import java.io.OutputStream
 import java.util.*
 import kotlin.concurrent.thread
 import kotlin.coroutines.resume
@@ -44,38 +46,62 @@ class GoogleDriveUseCase constructor(
         get() = GoogleSignIn.getLastSignedInAccount(context)
 
 
-    suspend fun createFile(): String = withContext(Dispatchers.IO) {
-        suspendCoroutine { continuation ->
-            thread {
-                try {
-                    val metadata: File = File()
-                        .setParents(Collections.singletonList("root"))
-                        .setMimeType("text/plain")
-                        .setName("Untitled file")
-                    val driveFile = driveService().files().create(metadata).execute()
-                    continuation.resume(driveFile.id)
-                } catch (t: Throwable) {
-                    continuation.resumeWithException(t)
+    suspend fun saveFile(
+        name: String,
+        path: String,
+        mimeType: String,
+        fileId: String? = null
+    ): File {
+        val metadata = File().setName(name)
+
+        val filePath = java.io.File(path)
+        val mediaContent = FileContent(mimeType, filePath)
+
+        return saveFile(metadata, mediaContent)
+    }
+
+    suspend fun saveFile(metadata: File, mediaContent: FileContent, fileId: String? = null): File =
+        withContext(Dispatchers.IO) {
+            suspendCoroutine { continuation ->
+                thread {
+                    try {
+                        val driveFile = if (fileId == null) {
+                            driveService().files().create(metadata, mediaContent)
+                                .setFields("id")
+                                .execute()
+                        } else {
+                            driveService().files().update(fileId, metadata, mediaContent).execute()
+                        }
+
+                        continuation.resume(driveFile)
+                    } catch (t: Throwable) {
+                        continuation.resumeWithException(t)
+                    }
                 }
             }
         }
-    }
 
-    suspend fun saveFile(fileId: String, name: String, content: String): Unit = withContext(Dispatchers.IO) {
-        suspendCoroutine { continuation ->
-            thread {
-                try {
-                    val metadata = File().setName(name)
-                    val contentStream = ByteArrayContent.fromString("text/plain", content)
-                    driveService().files().update(fileId, metadata, contentStream).execute()
-                    continuation.resume(Unit)
-                } catch (t: Throwable) {
-                    continuation.resumeWithException(t)
+    suspend fun getFile(fileId: String, fileName: String): java.io.File =
+        withContext(Dispatchers.IO) {
+            suspendCoroutine { continuation ->
+                thread {
+                    try {
+                        val outFile = java.io.File(context.cacheDir, fileName)
+
+                        val outStream: OutputStream = FileOutputStream(outFile)
+
+                        driveService().files()[fileId]
+                            .executeMediaAndDownloadTo(outStream)
+
+                        outStream.close()
+
+                        continuation.resume(outFile)
+                    } catch (t: Throwable) {
+                        continuation.resumeWithException(t)
+                    }
                 }
             }
         }
-
-    }
 
     suspend fun listFiles(): FileList = withContext(Dispatchers.IO) {
         suspendCoroutine { continuation ->
