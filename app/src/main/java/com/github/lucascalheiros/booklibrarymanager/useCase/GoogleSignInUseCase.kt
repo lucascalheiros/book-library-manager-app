4a389faaf90f65ea285125a210f3f685b2f04ac4
Lucas Calheiros <lucascalheirosjllc@gmail.com>
2022-09-16 19:05:53 -0300

    refact: Use interface in GoogleSignInUseCase

--- app/src/main/java/com/github/lucascalheiros/booklibrarymanager/useCase/GoogleSignInUseCase.kt
+++ app/src/main/java/com/github/lucascalheiros/booklibrarymanager/useCase/GoogleSignInUseCase.kt
@@ -1,51 +1,10 @@
 package com.github.lucascalheiros.booklibrarymanager.useCase
 
-
-import android.content.Context
-import android.content.Intent
-import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.Scopes
-import com.google.android.gms.common.api.Scope
-import kotlinx.coroutines.tasks.await
-import org.koin.core.annotation.Single
 
-@Single
-class GoogleSignInUseCase constructor(
-    private val context: Context
-) {
-    private val googleSignInOptions by lazy {
-        GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .requestScopes(Scope(Scopes.DRIVE_FILE))
-            .build()
-    }
-
-    private val googleSignInClient by lazy { GoogleSignIn.getClient(context, googleSignInOptions) }
-
+interface GoogleSignInUseCase {
     val signedInAccount: GoogleSignInAccount?
-        get() = GoogleSignIn.getLastSignedInAccount(context)
-
     val isUserSignedIn: Boolean
-       get() = signedInAccount != null
-
-    suspend fun signIn(): SignInRequestState {
-        return try {
-            val account = googleSignInClient.silentSignIn().await()
-            SignInRequestState.Signed(account)
-        } catch (t: Exception) {
-            SignInRequestState.Unsigned(googleSignInClient.signInIntent)
-        }
-    }
-
-    fun signOut() {
-        googleSignInClient.signOut()
-    }
-
-}
-
-sealed class SignInRequestState {
-    data class Signed(val account: GoogleSignInAccount): SignInRequestState()
-    data class Unsigned(val signAccountIntent: Intent): SignInRequestState()
+    suspend fun signIn(): SignInRequestState
+    suspend fun signOut()
 }
